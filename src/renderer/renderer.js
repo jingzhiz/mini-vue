e35975280673e5f97c9d953bf93d4b4b773c33d1
jingzhi99 <623292519@qq.com>
2023-10-31 20:02:51 +0800

    feature# 事件的处理以及文本/注释/Fragment的处理

--- src/renderer/renderer.js
+++ src/renderer/renderer.js
@@ -1,9 +1,17 @@
+// 特殊节点类型的标注
+const Text = Symbol('Text')
+const Comment = Symbol('Comment')
+const Fragment = Symbol('Fragment')
+
 // 使用生成渲染器函数，可以生成不同类型的渲染器
 function createRenderer(options = {}) {
   // 针对不同平台的统一 API 风格处理
   const {
     createElement,
+    createComment,
     setElementText,
+    createTextNode,
+    setText,
     insert,
     patchProps
   } = options
@@ -36,7 +44,55 @@ function createRenderer(options = {}) {
 
   // 更新普通标签元素
   function patchElement(n1, n2) {
+    const el = n2.el = n1.el
+    const oldProps = n1.props
+    const newProps = n2.props
 
+    // 新旧 props 相互比较进行更新
+    for (const key in newProps) {
+      if (newProps[key] !== oldProps[key]) {
+        patchProps(el, key, oldProps[key], newProps[key])
+      }
+    }
+    for (const key in oldProps) {
+      if (!(key in newProps)) {
+        patchProps(el, key, oldProps[key], null)
+      }
+    }
+
+    // 更新子节点
+    patchChildren(n1, n2, el)
+  }
+
+  function patchChildren(n1, n2, container) {
+    if (isString(n2.children)) {
+      // 如果 children 是 String 表示更新为文本节点
+      if (isArray(n1.children)) {
+        // 先卸载所有子元素
+        n1.children.forEach(child => unmount(child));
+      }
+      // 在将文本设置成节点
+      setElementText(container, n2.children)
+    } else if (isArray(n2.children)) {
+      // 如果是数组表示为一组子节点
+      if (isArray(n1.children)) {
+        n1.children.forEach(child => unmount(child));
+        n2.children.forEach(child => patch(null, child, container))
+      } else {
+        // n1.children 是文本节点，则清空文本然后遍历 n2.children 进行挂载
+        setElementText(container, '')
+        n2.children.forEach(child => patch(null, child, container))
+      }
+    } else {
+      // 此分支表示未存在 n2.children
+      if (isArray(n1.children)) {
+        n1.children.forEach(child => unmount(child))
+      } else if (isString(n1.children)) {
+        setElementText(container, '')
+      } else {
+        // 在这里则表示 n1 和 n2 都无 children
+      }
+    }
   }
   
   // 挂载组件
@@ -48,6 +104,12 @@ function createRenderer(options = {}) {
   }
 
   function unmount(vnode) {
+    // 如果是 Fragment 时，只需要卸载 children 即可
+    if (vnode.type === Fragment) {
+      vnode.children.forEach(child => unmount(child))
+      return
+    }
+
     const parent = vnode.el.parentNode
     if (parent) parent.removeChild(vnode.el)
   }
@@ -70,8 +132,35 @@ function createRenderer(options = {}) {
         // 更新
         patchElement(n1, n2)
       }
-    } else if (isObject(type)) {
-      // mountComponent()
+    } else if (type === Text) {
+      // 文本节点的更新
+      if (!n1) {
+        const el = n2.el = createTextNode(n2.children)
+        insert(el, container)
+      } else {
+        const el = n2.el = n1.el
+        if (n2.children !== n1.children) {
+          setText(el, n2.children)
+        }
+      }
+    } else if (type === Comment) {
+      // 注释类型的更新
+      if (!n1) {
+        const el = n2.el = createComment(n2.children)
+        insert(el, container)
+      } else {
+        const el = n2.el = n1.el
+        if (n2.children !== n1.children) {
+          setText(el, n2.children)
+        }
+      }
+    } else if (type === Fragment) {
+      // Fragment 片段只要关注 children 即可
+      if (!n1) {
+        n2.children.forEach(child => patch(null, child, container))
+      } else {
+        patchChildren(n1, n2, container)
+      }
     }
 
   }
@@ -97,8 +186,17 @@ const { render } = createRenderer({
   createElement(type) {
     return document.createElement(type)
   },
+  createTextNode(text) {
+    return document.createTextNode(text)
+  },
+  createComment(comment) {
+    return document.createComment(comment)
+  },
   setElementText(el, text) {
     el.textContent = text
+  },
+  setText(el, text) {
+    el.nodeValue = text
   },
   insert(el, parent, anchor = null) {
     parent.insertBefore(el, anchor)
@@ -114,6 +212,8 @@ const { render } = createRenderer({
         // 如果没有 invoker, 则伪造一个 invoker 缓存到 el._vei
         if (!invoker) {
           invoker = el._vei[key] = (e) => {
+            // 如果事件的发生时间小于事件处理函数绑定的时间，则不执行处理函数
+            if (e.timeStamp < invoker.attached) return
             // 当伪造的事件处理函数执行时，执行真正的函数
             if (isArray(invoker.value)) {
               invoker.value.forEach(fn => fn(e))
@@ -122,6 +222,7 @@ const { render } = createRenderer({
             }
           }
           invoker.value = nextValue
+          invoker.attached = performance.now()
           el.addEventListener(eventName, invoker)
         }
       } else if (invoker) {
@@ -141,6 +242,14 @@ const { render } = createRenderer({
         Object.entries(value).forEach(([k, v]) => {
           el.style[k] = v
         })
+      } else {
+        if (isString) {
+          el.style.cssText = ''
+        } else if (isObject(prevValue)) {
+          Object.entries(prevValue).forEach(([k, v]) => {
+            el.style[k] = ''
+          })
+        }
       }
     } else if (shouldSetAsProps(el, key, nextValue)) {
       if (isBoolean(el[key]) && value === '') {
